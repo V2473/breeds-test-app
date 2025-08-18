ec75e930584f7e6e6a503df6be967c019e6966ad
VolodymyrS <vvsefrv@gmail.com>
2025-08-18 09:05:14 +0300

    lint

--- src/app/[breed]/page.tsx
+++ src/app/[breed]/page.tsx
@@ -2,8 +2,9 @@
 
 "use client";
 import { use, useEffect, useState } from 'react'
- 
-export default function BreedPage({params}: {params: Promise<{ breed: string }>
+
+export default function BreedPage({ params }: {
+  params: Promise<{ breed: string }>
 }) {
 
   const [breedInfo, setBreedInfo] = useState<string>('');
@@ -13,19 +14,19 @@ export default function BreedPage({params}: {params: Promise<{ breed: string }>
   const id = breed.split('-')[0];
   const breedName = breed.split('-').slice(1).join(' ');
   const API_KEY = id.length > 3 ? process.env.CAT_API_KEY : process.env.DOG_API_KEY;
-  const infoUrl = id.length > 3 ? 
-    `https://api.thecatapi.com/v1/breeds/${id}` : 
+  const infoUrl = id.length > 3 ?
+    `https://api.thecatapi.com/v1/breeds/${id}` :
     `https://api.thedogapi.com/v1/breeds/${id}`;
-  const imgUrl = id.length > 3 ? 
-    `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${id}` : 
+  const imgUrl = id.length > 3 ?
+    `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${id}` :
     `https://api.thedogapi.com/v1/images/search?limit=10&breed_ids=${id}`;
-  const imgUrlDirect = id.length > 3 ? 
-    'https://cdn2.thecatapi.com/images/' : 
+  const imgUrlDirect = id.length > 3 ?
+    'https://cdn2.thecatapi.com/images/' :
     'https://cdn2.thedogapi.com/images/';
   const breedDataText = id.length > 3 ? 'description' : `temperament`;
 
 
-  
+
   useEffect(() => {
     const fetchBreedDetails = async () => {
       try {
@@ -39,56 +40,57 @@ export default function BreedPage({params}: {params: Promise<{ breed: string }>
         const breedInfoFetched = await responseBreedDetails.text();
 
         setBreedInfo(breedInfo => breedInfo + breedInfoFetched);
-      } 
-        catch (error: Error | unknown) { if (error instanceof Error) console.error('Error fetching breed details:', error); }
-      };
-    
-        fetchBreedDetails();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+      }
+      catch (error: Error | unknown) { if (error instanceof Error) console.error('Error fetching breed details:', error); }
+    };
 
-    useEffect(() => {
-      const fetchBreedImageUrls = async () => {
-         try {
+    fetchBreedDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const fetchBreedImageUrls = async () => {
+      try {
         const imgLinksResponse = await fetch(imgUrl)
-        if (!imgLinksResponse.ok) { throw new Error(`Failed to fetch breed image, ${imgLinksResponse.status}`); } 
+        if (!imgLinksResponse.ok) { throw new Error(`Failed to fetch breed image, ${imgLinksResponse.status}`); }
         const imgDataFetch = await imgLinksResponse.text();
         setImgData(imgData => imgData + imgDataFetch);
       }
-        catch (error: Error | unknown) { if (error instanceof Error) console.error('Error fetching breed image:', error); }
-      };
-      fetchBreedImageUrls();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-   
-      //Breed info corrupted Json parsing
-      const descriptionStart = breedInfo.indexOf('"', breedInfo.indexOf(`${breedDataText}`) + 12);
-      const descriptionEnd = breedInfo.substring(descriptionStart + 1, breedInfo.indexOf('"', descriptionStart + 1));
-      const breedDescrioptionData = `Breed: ${breedName}. \nDescription: ${descriptionEnd}`;
+      catch (error: Error | unknown) { if (error instanceof Error) console.error('Error fetching breed image:', error); }
+    };
+    fetchBreedImageUrls();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-      //Image data corrupted Json parsing
-      const allPhotos: Array<string>= [];
-      let imgDataParsedFull: string = imgData;
-      let imgDataParsed: string = '';
-      let imgDataStart = 0;
-      let imgUrlParsed = '';
+  //Breed info corrupted Json parsing
+  const descriptionStart = breedInfo.indexOf('"', breedInfo.indexOf(`${breedDataText}`) + 12);
+  const descriptionEnd = breedInfo.substring(descriptionStart + 1, breedInfo.indexOf('"', descriptionStart + 1));
+  const breedDescrioptionData = `Breed: ${breedName}. \nDescription: ${descriptionEnd}`;
 
-      while (imgDataParsedFull.includes('id"')) {
-      imgDataStart = imgDataParsedFull.indexOf('id"') + 5;
-      imgUrlParsed = imgDataParsedFull.substring(imgDataStart, imgDataStart + 9);
-      if (imgUrlParsed.length > 8 ) {
-        imgDataParsed = imgDataParsed + "," + imgUrlParsed;
-      } imgDataParsedFull = imgDataParsedFull.slice(imgDataStart ); }
-      imgDataParsed.split(',').forEach((imgUrl) => {
-      if (imgUrl.length > 8) { allPhotos.push(imgUrlDirect + imgUrl + '.jpg'); }
-      });
+  //Image data corrupted Json parsing
+  const allPhotos: Array<string> = [];
+  let imgDataParsedFull: string = imgData;
+  let imgDataParsed: string = '';
+  let imgDataStart = 0;
+  let imgUrlParsed = '';
 
-      return (
-        <div className="breed-page flex flex-wrap flex-row items-center justify-center p-4 bg-gray-100">
-          <p className='w-100'>{breedDescrioptionData}</p>
-          {allPhotos.length > 0 ? allPhotos.map((img, index) => (
-            <img key={`${breedName}-${index}`} src={img} alt={`Image of ${breedName}`} className="max-h-80 object-cover w-80 m-2" />  ) 
-          ) : <p>No images available for this breed.</p>}
-        </div>
-      )
-   }
+  while (imgDataParsedFull.includes('id"')) {
+    imgDataStart = imgDataParsedFull.indexOf('id"') + 5;
+    imgUrlParsed = imgDataParsedFull.substring(imgDataStart, imgDataStart + 9);
+    if (imgUrlParsed.length > 8) {
+      imgDataParsed = imgDataParsed + "," + imgUrlParsed;
+    } imgDataParsedFull = imgDataParsedFull.slice(imgDataStart);
+  }
+  imgDataParsed.split(',').forEach((imgUrl) => {
+    if (imgUrl.length > 8) { allPhotos.push(imgUrlDirect + imgUrl + '.jpg'); }
+  });
+
+  return (
+    <div className="breed-page flex flex-wrap flex-row items-center justify-center p-4 bg-gray-100">
+      <p className='w-100'>{breedDescrioptionData}</p>
+      {allPhotos.length > 0 ? allPhotos.map((img, index) => (
+        <img key={`${breedName}-${index}`} src={img} alt={`Image of ${breedName}`} className="max-h-80 object-cover w-80 m-2" />)
+      ) : <p>No images available for this breed.</p>}
+    </div>
+  )
+}
